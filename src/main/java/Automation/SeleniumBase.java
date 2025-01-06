d35fa0c8472c020f43196db90ed624130d736299
BommuTanuja <tanuja.bommu12@gmail.com>
2025-01-06 14:17:59 +0530

    screenshots added

--- src/main/java/Automation/SeleniumBase.java
+++ src/main/java/Automation/SeleniumBase.java
@@ -1,14 +1,17 @@
 package Automation;
 
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.io.IOException;
 
 public class SeleniumBase {
     public WebDriver driver;
@@ -31,12 +34,23 @@ public class SeleniumBase {
     @BeforeSuite
     public void setUp(String browser){
         launchBrowser(browser);
+        driver.manage().window().maximize();
+
     }
 
     @Parameters("url")
     @BeforeTest
     public void launchApplication(String Url){
         driver.get(Url);
+    }
+
+    @BeforeMethod
+    public void takingScreenshot() throws IOException {
+        TakesScreenshot takesScreenshot=(TakesScreenshot) driver;
+        File sourceFile=takesScreenshot.getScreenshotAs(OutputType.FILE);
+        File targetFile=new File(System.getProperty("user.dir")+"\\screenShots\\loginFailed.png");
+
+        FileUtils.copyFile(sourceFile,targetFile);
     }
     @AfterSuite
     public void closeSession(){
